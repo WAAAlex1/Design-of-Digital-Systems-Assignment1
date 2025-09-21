d990e67f6a1928e917bd82e00b712f61a258a7fc
sadafayubb <130761527+sadafayubb@users.noreply.github.com>
2025-09-21 19:07:03 +0200

    Updated gcd.sv
    fixed syntax mistakes and fsm state transitions though it doesnt completely pass testbench

--- src/Task 2/gcd.sv
+++ src/Task 2/gcd.sv
@@ -13,108 +13,99 @@
 
 
 module gcd (
-    input  logic          clk,    // The clock signal.
-    input  logic          reset,  // Reset the module.
-    input  logic          req,    // Start computation.
-    input  logic [15 : 0] AB,     // The two operands. One at a time.
-    output logic          ack,    // Input received / Computation is complete.
-    output logic [15 : 0] C       // The result.
+  input  logic        clk,
+  input  logic        reset,
+  input  logic        req,
+  input  logic [15:0] AB,
+  output logic        ack,
+  output logic [15:0] C
 );
-    typedef enum logic [3 : 0] {
-        idleA, sampleA, ackA, idleB, sampleB, compare1, compare2, calc, ackResult
-        } state_t; // Input your own state names here
 
-    shortint unsigned reg_a, next_reg_a, reg_b, next_reg_b;
-    shortint signed compareReg, next_reg_compare;
+  typedef enum logic [3:0] {
+    idleA=0, sampleA=1, ackA=2, idleB=3, sampleB=4, compare=5, subA=6, subB=7, ackResult=8
+  } state_t; // Input your own state names here
 
-    state_t current_state, next_state;
+  state_t state, next_state;
+  
+  shortint unsigned reg_a, next_reg_a;
+  shortint unsigned reg_b, next_reg_b;
 
     // Combinatorial logic
-    always_comb begin
+  always_comb begin
+    next_state = state;
+    next_reg_a = reg_a;
+    next_reg_b = reg_b;
+    ack = 1'b0;
+    C   = 16'd0;
 
-        next_state = current_state;
-        next_reg_a = reg_a;
-        next_reg_b = reg_b;
-        next_reg_compare = '0;
-        ack = '0;
-        C = 'Z;
+    case (state)
+      idleA: 
+        if (req) next_state = sampleA;
 
-        case (current_state)
-            idleA:
-                begin
-                    if(req) next_state = sampleA;
-                end
-            sampleA:
-                begin
-                    if(!req) next_state = ackA;
-                    next_reg_a = AB;
-                end
-            ackA:
-                begin
-                    next_state = idleB;
-                    ack = '1;
-                end
-            idleB:
-                begin
-                    if(req) next_state = sampleB;
-                    ack = '0;
-                end
-            sampleB:
-                begin
-                    next_state = compare1;
-                    next_reg_b = AB;
-                end
-            compare:
-                begin
-                    next_reg_compare = reg_a - reg_b;
-                    next_state = compare2;
-                end
-            compare2:
-                begin
-                    //If dif is 0 then skip to ackResult
-                    if(compareReg =='0) next_state = ackResult;
-                    else next_state = calc;
-                end
-            calc:
-                begin
-                    if(n) begin
-                        a = a-b;
-                    end else begin
-                        b = b-a;
+      sampleA: 
+      begin
+        next_reg_a = AB;
+        next_state = ackA;
+      end
 
+      ackA: 
+      begin
+        ack = 1'b1;
+        if (!req) next_state = idleB; 
+      end
 
-                    /*
-                    if(a !== b) begin
-                        if(a > b) a = a-b;
-                        else b = b-a;
-                        next_state = calc;
-                    end else begin
-                        next_state = ackResult;
-                    end
-                    */
-                end
-            ackResult:
-                begin
-                    ack = '1;
-                    C = a;
-                    next_state = idleA;
-                end
-            default: next_state = idleA;
-        endcase
+      idleB: 
+      begin
+        if (req) next_state = sampleB;
+      end  
+
+      sampleB: 
+      begin
+          next_reg_b = AB;
+          next_state = compare;
+      end
+
+      compare: 
+      begin
+        if (reg_a == reg_b) next_state = ackResult;
+        else if (reg_a > reg_b) next_state = subA;
+        else next_state = subB;
+      end
+
+      subA: 
+      begin
+        next_reg_a = reg_a - reg_b;
+        next_state = compare;
+      end
+
+      subB: 
+      begin
+        next_reg_b = reg_b - reg_a;
+        next_state = compare;
+      end
+
+      ackResult: 
+      begin
+        ack = 1'b1;
+        C = reg_a;
+        if (!req) next_state = idleA; 
+      end
+
+      default: next_state = idleA;
+    endcase
+  end
+
+  // Registers
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      state <= idleA;
+      reg_a <= 16'd0;
+      reg_b <= 16'd0;
+    end else begin
+      state <= next_state;
+      reg_a <= next_reg_a;
+      reg_b <= next_reg_b;
     end
+  end
 
-    // Registers
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
-            current_state <= idleA;
-            reg_a <= '0;
-            reg_b <= '0;
-
-        end else begin
-            current_state <= next_state;
-            compareReg <= next_reg_compare;
-            reg_a <= next_reg_a;
-            reg_b <= next_reg_b;
-        end
-    end
 endmodule
